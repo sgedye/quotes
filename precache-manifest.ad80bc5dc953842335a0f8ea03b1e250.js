799619d9a96805fcacaae87a8db77c0b10d1290b
Shaun Gedye <sgedye88@yahoo.com.au>
2020-02-05 14:24:36 +1100

    Updates

---
+++ precache-manifest.ad80bc5dc953842335a0f8ea03b1e250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8de0ff189a82d390031a58341846cf",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "4fa30bbc27842f347495",
+    "url": "/quotes/static/css/main.e9d8a2a5.chunk.css"
+  },
+  {
+    "revision": "0cec8c14c83d97dd88b6",
+    "url": "/quotes/static/js/2.fec23751.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quotes/static/js/2.fec23751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa30bbc27842f347495",
+    "url": "/quotes/static/js/main.8ae5e362.chunk.js"
+  },
+  {
+    "revision": "a4cf80bcb5008db28233",
+    "url": "/quotes/static/js/runtime-main.1918892d.js"
+  }
+]);
